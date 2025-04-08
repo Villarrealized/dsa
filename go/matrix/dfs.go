fdb5028a518377e1bf79d6337bfc6f270dab0309
Nicholas Villarreal <nvillarreal@protonmail.com>
2025-04-07 18:01:33 -0600

    update description

--- go/matrix/dfs.go
+++ go/matrix/dfs.go
@@ -3,7 +3,9 @@ package matrix
 type Matrix [][]uint8
 
 /*
-Count the unique paths from the top left to the bottom right. A single path may only move along 0's and can't visit the same cell more than once
+You are given a binary matrix Grid where 0s represent land and 1s represent rocks that can not be traversed.
+
+Return the number of unique paths from the top-left corner of Grid to the bottom-right corner such that all traversed cells are land cells. You may only move vertically or horizontally through land cells. For an individual unique path you cannot visit the same cell twice.
 */
 func Dfs(grid Matrix) int {
 	rowLen := len(grid)
